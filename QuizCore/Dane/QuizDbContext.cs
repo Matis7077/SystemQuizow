a82bbead606d04badf36f4192db7b5a39f66b2f7
Maciej Grabarczyk <grabarczykmaciej96@gmail.com>
2025-01-11 15:00:00 +0100

    Konfiguracja Entity Framework Core i DbContext

---
+++ QuizCore/Dane/QuizDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizCore.Modele;
+
+namespace QuizCore.Dane
+{
+    public class QuizDbContext : DbContext
+    {
+        public DbSet<Quiz> Quizy { get; set; }
+        public DbSet<Pytanie> Pytania { get; set; }
+        public DbSet<Odpowiedz> Odpowiedzi { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=QuizyBaza;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Quiz>()
+                .HasMany(q => q.Pytania)
+                .WithOne(p => p.Quiz)
+                .HasForeignKey(p => p.QuizId);
+
+            modelBuilder.Entity<Pytanie>()
+                .HasMany(p => p.Odpowiedzi)
+                .WithOne(o => o.Pytanie)
+                .HasForeignKey(o => o.PytanieId);
+        }
+    }
+}
